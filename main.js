770deb26abfd3a65b8c90afaca55213f5fb4851c
thedteam <80823193+thedteam@users.noreply.github.com>
2024-02-11 21:15:27 +0530

    D

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("sw.js");
   }
   
+  /*=============== Offline Page ===============*/
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js');
+  }
+});
